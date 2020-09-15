1d3b77dbd0ce548352e8b68cc8b489b843a51b1b
denny.k <denny.k@kakaocorp.com>
2020-09-15 12:09:43 +0900

    Initial Commit

---
+++ ElliotableSwiftUI/ElliotableSwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  ElliotableSwiftUI.h
+//  ElliotableSwiftUI
+//
+//  Created by Denny on 2020/09/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ElliotableSwiftUI.
+FOUNDATION_EXPORT double ElliotableSwiftUIVersionNumber;
+
+//! Project version string for ElliotableSwiftUI.
+FOUNDATION_EXPORT const unsigned char ElliotableSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElliotableSwiftUI/PublicHeader.h>
+
+
